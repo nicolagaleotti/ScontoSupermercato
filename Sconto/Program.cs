f75a25f74054d459aaaded0095e5602e77c5f095
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:02:55 +0200

    fine programma

--- Sconto/Program.cs
+++ Sconto/Program.cs
@@ -6,7 +6,19 @@ namespace Sconto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Totale della spesa? ");
+            double totale = double.Parse(Console.ReadLine());
+            double scontato;
+            if (totale > 100)
+            {
+                scontato = totale - totale / 100 * 20;
+                Console.WriteLine($"L'importo scontato è {scontato}");
+            }
+            else
+            {
+                Console.WriteLine("Non è applicabile lo sconto");
+            }
+            Console.ReadLine();
         }
     }
 }
