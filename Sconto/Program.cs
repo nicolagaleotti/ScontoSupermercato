d64928b4bc9f8d215d9a95dbcca07b471de7de53
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:06:32 +0200

    Aggiunta prezzo da pagare

--- Sconto/Program.cs
+++ Sconto/Program.cs
@@ -12,11 +12,11 @@ namespace Sconto
             if (totale > 100)
             {
                 scontato = totale - totale / 100 * 20;
-                Console.WriteLine($"L'importo scontato è {scontato}");
+                Console.WriteLine($"L'importo scontato è {scontato} euro");
             }
             else
             {
-                Console.WriteLine("Non è applicabile lo sconto");
+                Console.WriteLine($"Non è applicabile lo sconto. Paghi {totale} euro");
             }
             Console.ReadLine();
         }
