1b545ecc39aeb30f0f98418b306986cb28e161ee
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 11:55:57 +0200

    Add project files.

---
+++ Sconto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
